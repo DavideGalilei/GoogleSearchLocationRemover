54f91a702f2c2ff9e839484b0682186815f823cd
Davide Galilei <43778739+DavideGalilei@users.noreply.github.com>
2021-09-18 10:00:03 +0200

    Initial Commit

---
+++ google_search_location_remover.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Google search location remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove google search location banner from the bottom of the results page
+// @author       Davide Galilei
+// @match        https://www.google.com/search*
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let x = document.getElementById("footcnt");
+    x.firstElementChild.firstElementChild.firstElementChild.remove();
+})();
